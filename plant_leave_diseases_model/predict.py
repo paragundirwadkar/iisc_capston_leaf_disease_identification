c466703ac04a060e4020bbcdac1c38071ccf6741
parag u <uparag@googlemail.com>
2023-12-09 10:31:18 +0530

    master_class: prediction

--- plant_leave_diseases_model/predict.py
+++ plant_leave_diseases_model/predict.py
@@ -11,8 +11,8 @@ import tensorflow as tf
 
 from plant_leave_diseases_model import __version__ as _version
 from plant_leave_diseases_model.config.core import TRAINED_MODEL_DIR, config
-from plant_leave_diseases_model.processing.data_manager import convert_image_to_array, get_model_file_name_path, load_leaf_disease_dataset, load_model, load_test_dataset
-from plant_leave_diseases_model.processing.data_setup import test_directory
+from plant_leave_diseases_model.processing.data_manager import convert_image_to_array, get_class_file_list, get_model_file_name_path, load_leaf_disease_dataset, load_model, load_test_dataset
+from plant_leave_diseases_model.processing.data_setup import test_directory,class_file_path
 
 #model_file_name = f"{config.app_config.model_save_file}{_version}"
 #clf_model = load_model(file_name = model_file_name)
@@ -42,13 +42,26 @@ def make_prediction(*, test_dir_img_file_path) -> dict:
     
     predictions = model.predict(np_image_list,verbose = 0)
     results = {"predictions": None, "version": _version}
-
+    ###########################################
+    # Geting array of master class array
+    ###########################################
+    master_class_arr=get_class_file_list(class_file_path)
+    print("master_class_arr:",master_class_arr)
+    
+    print(predictions)
+    
+    for pred in predictions:
+        np.multiply(np.array(pred), 100000)
+        max_index = np.multiply(np.array(pred), 100000).argmax()
+        print("max_index::",max_index,"master_class_arr:",master_class_arr[max_index])
+        
+        
     # pred_labels = []
     # for i in predictions:
     #     pred_labels.append(config.model_config.label_mappings[int(predictions + 0.5)])
         
     #results = {"predictions": pred_labels, "version": _version}
-    print(predictions)
+    
 
     return predictions
 
